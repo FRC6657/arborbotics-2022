bb389994604b905e301d7919902160fef99a0fc1
Andrew Card <mrsaltinecracker1@gmail.com>
2022-01-04 18:26:14 -0800

    Comments

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -15,26 +15,34 @@ import edu.wpi.first.wpilibj.XboxController.Axis;
 import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
-
+/**
+ * This is completly temporary and is only really to have drivetrain code day 1.
+ */
 public class Robot extends TimedRobot {
 
+  //4 Drivetrain Motors
   private WPI_TalonFX mFrontLeft = new WPI_TalonFX(1);
   private WPI_TalonFX mFrontRight = new WPI_TalonFX(2);
   private WPI_TalonFX mBackLeft = new WPI_TalonFX(3);
   private WPI_TalonFX mBackRight = new WPI_TalonFX(4);
-
+  
+  //DifferentialDrive class to make things easy
   private DifferentialDrive mDrivetrain = new DifferentialDrive(mFrontLeft, mFrontRight);
+
+  //Max set percent
   private double speed = 1;
 
+  //Xbox Controllers are typically easier to test with
   private XboxController mController = new XboxController(0);
 
   @Override
   public void robotInit() {
-    configureMotors();
+    configureMotors();//Configure all of the motors
   }
 
   @Override
   public void teleopPeriodic() {
+    //Telop Controlls
     mDrivetrain.curvatureDrive(
       deadband(-mController.getRawAxis(Axis.kLeftY.value), 0.1) * speed,
       deadband(mController.getRawAxis(Axis.kRightX.value), 0.1) * speed,
@@ -44,32 +52,39 @@ public class Robot extends TimedRobot {
 
   @Override
   public void disabledInit() {
+    //Stop the drivetrain
     mDrivetrain.stopMotor();
   }
 
   private void configureMotors() {
 
+    //Reset all of the motors to default values
     mFrontLeft.configFactoryDefault();
     mFrontRight.configFactoryDefault();
     mBackLeft.configFactoryDefault();
     mBackRight.configFactoryDefault();
 
+    //Set the back motors to follow the commands of the front
     mBackLeft.follow(mFrontLeft);
     mBackRight.follow(mFrontRight);
 
+    //Set the neutral modes
     mFrontLeft.setNeutralMode(NeutralMode.Brake);
     mFrontRight.setNeutralMode(NeutralMode.Brake);
     mBackLeft.setNeutralMode(NeutralMode.Brake);
     mBackRight.setNeutralMode(NeutralMode.Brake);
 
+    //Makes all positive signals move the robot forward
     mFrontLeft.setInverted(TalonFXInvertType.Clockwise);
     mBackLeft.setInverted(TalonFXInvertType.Clockwise);
     mFrontRight.setInverted(TalonFXInvertType.CounterClockwise);
     mBackRight.setInverted(TalonFXInvertType.CounterClockwise);
 
+    //Enables the encoders for ctre special stuff 
     mFrontLeft.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor);
     mFrontRight.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor);
 
+    //Limits the current to prevent breaker tripping
     mFrontLeft.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, 40, 45, 0.5)); //| Enabled | 40a Limit | 45a Thresh | .5 sec Trigger Time
     mFrontRight.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, 40, 45, 0.5)); //| Enabled | 40a Limit | 45a Thresh | .5 sec Trigger Time
     mBackLeft.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, 40, 45, 0.5)); //| Enabled | 40a Limit | 45a Thresh | .5 sec Trigger Time
