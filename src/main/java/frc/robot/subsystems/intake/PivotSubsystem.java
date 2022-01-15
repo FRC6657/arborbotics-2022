3f65222ac211d4e2e03749011a738740a31fa3ff
scottwegley <scottwegley@gmail.com>
2022-01-15 13:36:32 -0800

    Setup PivotSubsystem
    Co-authored-by: Addie Reardon <areardon3@users.noreply.github.com>

---
+++ src/main/java/frc/robot/subsystems/intake/PivotSubsystem.java
@@ -0,0 +1,19 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.intake;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import io.github.oblarg.oblog.Loggable;
+import frc.robot.Constants;
+import io.github.oblarg.oblog.annotations.Config;
+
+public class PivotSubsystem extends SubsystemBase implements Loggable {
+  public PivotSubsystem() {}
+
+  @Override
+  public void periodic() {
+    
+  }
+}
