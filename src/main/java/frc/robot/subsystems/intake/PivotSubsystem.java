15a14edf1d68f6c7f8c9cc57610120641e3daaf4
scottwegley <scottwegley@gmail.com>
2022-01-15 14:13:22 -0800

    Added solenoid and commands

--- src/main/java/frc/robot/subsystems/intake/PivotSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/PivotSubsystem.java
@@ -4,17 +4,40 @@
 
 package frc.robot.subsystems.intake;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import io.github.oblarg.oblog.Loggable;
 import frc.robot.Constants;
 import io.github.oblarg.oblog.annotations.Config;
 
 public class PivotSubsystem extends SubsystemBase implements Loggable {
-  
-  public PivotSubsystem() {}
+
+  private final DoubleSolenoid extender = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1);
+
+  public PivotSubsystem() {
+  }
 
   @Override
   public void periodic() {
-    
+
+  }
+
+  private final void configExtender() {
+    extender.set(Value.kReverse);
+  }
+
+  private final void toggle() {
+    extender.toggle();
+  }
+
+  public class toggleCommand extends InstantCommand {
+
+    @Override
+    public final void initialize() {
+      toggle();
+    }
   }
 }
