ecc56fae187a06a0bcac25be869d3ba5b05fffc7
scottwegley <scottwegley@gmail.com>
2022-01-15 13:40:58 -0800

    Created pneumatics controller

--- src/main/java/frc/robot/subsystems/pneumatics/PneumaticsController.java
+++ src/main/java/frc/robot/subsystems/pneumatics/PneumaticsController.java
@@ -2,19 +2,17 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems.intake;
+package frc.robot.subsystems.pneumatics;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import io.github.oblarg.oblog.Loggable;
-import frc.robot.Constants;
-import io.github.oblarg.oblog.annotations.Config;
 
-public class PivotSubsystem extends SubsystemBase implements Loggable {
-  
-  public PivotSubsystem() {}
+public class PneumaticsController extends SubsystemBase implements Loggable{
+
+  public PneumaticsController() {}
 
   @Override
   public void periodic() {
-    
+
   }
 }
