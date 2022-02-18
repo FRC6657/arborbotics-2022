cd53adbe271d9b3b24e837cdb81c0ee300e58ef3
Andrew Card <mrsaltinecracker1@gmail.com>
2022-02-18 06:57:06 -0800

    Added hood angle to Lerp

--- src/main/java/frc/FRC6657/custom/ArborMath.java
+++ src/main/java/frc/FRC6657/custom/ArborMath.java
@@ -4,4 +4,7 @@ public class ArborMath {
     public static double signumPow(double value, double power){
         return Math.signum(value) * Math.pow(Math.abs(value),power);
     }
+    public static boolean inRange(double val, double lower, double upper) {
+        return !(lower <= val && upper < val);
+    }
 }
