4f2269957b54da2193d20c13ed453900c879dcbc
addierrr <aereardon06@gmail.com>
2022-02-18 09:06:40 -0800

    Removed unused intake imports because green squiggles are ugly

--- src/main/java/frc/FRC6657/subsystems/intake/PickupSubsystem.java
+++ src/main/java/frc/FRC6657/subsystems/intake/PickupSubsystem.java
@@ -5,8 +5,6 @@
 package frc.FRC6657.subsystems.intake;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
-import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
