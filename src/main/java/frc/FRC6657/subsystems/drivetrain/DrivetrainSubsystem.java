30b52be002645bc0dc8986dffbb68941888ce95d
IShaLinI <mrsaltinecracker1@gmail.com>
2022-02-28 09:11:26 -0800

    Made Auto's repeatable

--- src/main/java/frc/FRC6657/subsystems/drivetrain/DrivetrainSubsystem.java
+++ src/main/java/frc/FRC6657/subsystems/drivetrain/DrivetrainSubsystem.java
@@ -473,6 +473,7 @@ public class DrivetrainSubsystem extends SubsystemBase implements Loggable{
       mRobotPath.setPoses(mPathPoints);
       mTrajectoryPlot.setTrajectory(trajectory);
 
+      timer.reset();
       timer.start();
     }
 
