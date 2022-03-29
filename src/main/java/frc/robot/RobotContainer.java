fb10316c6fdf4116b0769c5c3c826a12b799b8ef
ShaLin <mrsaltinecracker1@gmail.com>
2022-03-29 08:31:27 -0700

    Firing Button

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,9 +10,11 @@ import edu.wpi.first.wpilibj.smartdashboard.FieldObject2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.RunEndCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
@@ -105,36 +107,96 @@ public class RobotContainer {
           )
           ).andThen(new InstantCommand(() -> vision.visionSupplier.disableLEDs()))
     );
+    
+    // mDriverController.x().whenHeld(
+    //   new ParallelCommandGroup(
+    //     new SequentialCommandGroup(
+    //       new WaitUntilCommand(() -> (hood.ready() && flywheel.ready())),
+    //       new ParallelCommandGroup(
+    //         new StartEndCommand(accelerator::start, accelerator::stop, accelerator),
+    //         new StartEndCommand(pistons::extend, pistons::retract, pistons)
+    //       )
+    //     ),
+    //     new ParallelCommandGroup(
+    //       new RunEndCommand(() -> flywheel.setTargetRPM(2500), flywheel::stop, flywheel),
+    //       new RunEndCommand(() -> hood.setTargetAngle(1), hood::stop, hood)
+    //     )
+    //     .beforeStarting(
+    //       new SequentialCommandGroup(
+    //         new InstantCommand(() -> vision.visionSupplier.enableLEDs()),
+    //         new WaitCommand(0.25)
+    //       )
+    //     )
+    //     .andThen(
+    //       new InstantCommand(
+    //         () -> vision.visionSupplier.disableLEDs()
+    //       )
+    //     )
+    //   )
+    // );
 
-    
-    mDriverController.y().whenHeld(
-      new StartEndCommand(accelerator::start, accelerator::stop, accelerator)
+    // mDriverController.x().whenHeld(
+    //     new ParallelCommandGroup(//2500
+    //       new RunEndCommand(() -> flywheel.setTargetRPM(1000), flywheel::stop, flywheel),
+    //       new RunEndCommand(() -> hood.setTargetAngle(1), hood::stop, hood)
+    //     )
+    // );
+
+
+    mDriverController.pov.up().whenHeld(
+      new StartEndCommand(() -> lift.set(1), lift::stop, lift)
     );
-    
+
+    mDriverController.pov.down().whenHeld(
+      new StartEndCommand(() -> lift.set(-1), lift::stop, lift)
+    );
+
     mDriverController.x().whenHeld(
+      new SequentialCommandGroup(
+          new ParallelCommandGroup(//2500
+            new InstantCommand(() -> flywheel.setTargetRPM(2000), flywheel),
+            new InstantCommand(() -> hood.setTargetAngle(50), hood)
+          ),
+          new WaitUntilCommand(() -> flywheel.ready()),
+          new InstantCommand(pistons::extend, pistons),
+          new RunCommand(accelerator::start, accelerator).withInterrupt(() -> flywheel.shotDetector()),
+          new InstantCommand(pistons::retract, pistons),
+          new InstantCommand(() -> accelerator.set(-1)),
+          new WaitCommand(0.25),
+          new InstantCommand(accelerator::stop),
+          new WaitUntilCommand(() -> flywheel.ready()),
+          new RunCommand(accelerator::start)
+      )
+    ).whenReleased(
       new ParallelCommandGroup(
-        new SequentialCommandGroup(
-          new WaitUntilCommand(() -> (hood.ready() && flywheel.ready())),
-          new ParallelCommandGroup(
-            new StartEndCommand(accelerator::start, accelerator::stop, accelerator),
-            new StartEndCommand(pistons::extend, pistons::retract, pistons)
-          )
-        ),
-        new ParallelCommandGroup(
-          new RunEndCommand(() -> flywheel.setTargetRPM(2500), flywheel::stop, flywheel),
-          new RunEndCommand(() -> hood.setTargetAngle(1), hood::stop, hood)
-        )
-        .beforeStarting(
-          new SequentialCommandGroup(
-            new InstantCommand(() -> vision.visionSupplier.enableLEDs()),
-            new WaitCommand(0.25)
-          )
-        )
-        .andThen(
-          new InstantCommand(
-            () -> vision.visionSupplier.disableLEDs()
-          )
-        )
+        new InstantCommand(flywheel::stop, flywheel),
+        new InstantCommand(hood::stop, hood),
+        new InstantCommand(accelerator::stop, accelerator),
+        new InstantCommand(pistons::retract, pistons)
+      )
+    );
+
+    mDriverController.a().whenHeld(
+      new SequentialCommandGroup(
+          new ParallelCommandGroup(//2500
+            new InstantCommand(() -> flywheel.setTargetRPM(2500), flywheel),
+            new InstantCommand(() -> hood.setTargetAngle(50), hood)
+          ),
+          new WaitUntilCommand(() -> flywheel.ready()),
+          new InstantCommand(pistons::extend, pistons),
+          new RunCommand(accelerator::start, accelerator).withInterrupt(() -> flywheel.shotDetector()),
+          new InstantCommand(() -> accelerator.set(-1)),
+          new WaitCommand(0.25),
+          new InstantCommand(accelerator::stop),
+          new WaitUntilCommand(() -> flywheel.ready()),
+          new RunCommand(accelerator::start)
+      )
+    ).whenReleased(
+      new ParallelCommandGroup(
+        new InstantCommand(flywheel::stop, flywheel),
+        new InstantCommand(hood::stop, hood),
+        new InstantCommand(accelerator::stop, accelerator),
+        new InstantCommand(pistons::retract, pistons)
       )
     );
 
